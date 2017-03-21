b4a329874465ff76527573802f98d172d1ebe42e
Marc Magon <ged_marc@hotmail.com>
2017-03-21 20:41:57 +0200

    v0.9.1 description messages

--- src/za/co/mmagon/jwebswing/components/pace/PaceLoaderAttributes.java
+++ src/za/co/mmagon/jwebswing/components/pace/PaceLoaderAttributes.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (C) 2017 Marc Magon
  *
  * This program is free software: you can redistribute it and/or modify
@@ -15,8 +15,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package za.co.mmagon.jwebswing.components.pace;
-
-import za.co.mmagon.jwebswing.components.newcomponents.*;
 
 /**
  *
