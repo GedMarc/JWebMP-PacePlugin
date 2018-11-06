10babca52073f30c1cbb3be9a3c826ac7138c6a7
GedMarc <ged_marc@hotmail.com>
2018-11-06 21:29:31 +0200

    JRE 11 full split and module specifications a bit stricter

---
+++ src/main/java/com/jwebmp/plugins/pace/implementations/PaceExclusionsModule.java
@@ -0,0 +1,30 @@
+package com.jwebmp.plugins.pace.implementations;
+
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
+
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+public class PaceExclusionsModule
+		implements IGuiceScanModuleExclusions<PaceExclusionsModule>,
+				           IGuiceScanJarExclusions<PaceExclusionsModule>
+{
+
+	@Override
+	public @NotNull Set<String> excludeJars()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("jwebmp-pace-plugin-*");
+		return strings;
+	}
+
+	@Override
+	public @NotNull Set<String> excludeModules()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("com.jwebmp.plugins.pace");
+		return strings;
+	}
+}
